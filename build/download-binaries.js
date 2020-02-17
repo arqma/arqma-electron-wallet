24dd95e7fae5998590a7057e68c322ad30c352d7
ArqTras <33489188+ArqTras@users.noreply.github.com>
2020-02-18 00:23:12 +0100

    Update download-binaries.js

--- build/download-binaries.js
+++ build/download-binaries.js
@@ -18,7 +18,7 @@ async function download () {
             .map(asset => asset["browser_download_url"])
             .find(url => {
                 if (platform === "darwin") {
-                    return url.includes("osx") || url.includes("mac")
+                    return url.includes("apple") || url.includes("darwin14")
                 } else if (platform === "win32") {
                     return url.includes("win64") || url.includes("win64")
                 }
