859879f1927b7f1ae5a0d49a955a67306d625962
professorviz <musclesonvacation@hotmail.com>
2020-04-22 16:21:04 -0500

    removed uniform

--- src-electron/main-process/modules/pool/block.js
+++ src-electron/main-process/modules/pool/block.js
@@ -1,11 +1,10 @@
 import { randomBytes } from "crypto"
 
 export class Block {
-  constructor(pool, template, uniform=true) {
+  constructor(pool, template) {
 
       this.pool = pool
       this.template = template
-      this.uniform = uniform
       this.seed_hash = template.seed_hash
       this.next_seed_hash = template.next_seed_hash
       this.blockhashing_blob = template.blocktemplate_blob
@@ -14,24 +13,15 @@ export class Block {
       this.difficulty = template.difficulty
       this.address = pool.config.mining.address
 
-        this.buffer = Buffer.from(template.blocktemplate_blob, "hex")
+      this.buffer = Buffer.from(template.blocktemplate_blob, "hex")
 
-        if(uniform) {
-            /* Uniform mode
-             *   when enabled, we will mimic normal pool
-             *   set extra_nonce to random number between 0-31
-             *   also set "instanceID" to random four bytes
-             */
-            this.extra_nonce = randomBytes(1).readUInt8() % 32
-            randomBytes(4).copy(this.buffer, template.reserved_offset + 4)
-        }
+      this.extra_nonce = randomBytes(1).readUInt8() % 32
+      randomBytes(4).copy(this.buffer, template.reserved_offset + 4)
+
     }
     newBlob() {
         this.extra_nonce++
-        if(!this.uniform) {
-            this.extra_nonce = this.extra_nonce % 256
-        }
-        this.writeExtraNonce(this.extra_nonce)
+         this.writeExtraNonce(this.extra_nonce)
         return this.convertBlob()
     }
     convertBlob() {
@@ -45,11 +35,7 @@ export class Block {
         if(!buffer) {
             buffer = this.buffer
         }
-        if(this.uniform) {
-            buffer.writeUInt32BE(extra_nonce, this.template.reserved_offset)
-        } else {
-            buffer.writeUInt8(extra_nonce % 256, this.template.reserved_offset)
-        }
+        buffer.writeUInt32BE(extra_nonce, this.template.reserved_offset)
         return buffer
     }
 }
