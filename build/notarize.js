673d1b7005110b2fdab25d93efca5e9c5d2e2842
ArqTras <biuro@ar-net.info>
2020-06-14 22:01:33 +0200

    parent 0c434129a540f830b50bcf473fce7bd11aadb87c author ArqTras <biuro@ar-net.info> 1592164893 +0200 committer ArqTras <biuro@ar-net.info> 1594853310 +0200
    Revert "Update cs.js"
    
    This reverts commit 0c434129a540f830b50bcf473fce7bd11aadb87c.
    
    osx ?
    
    osx
    
    link
    
    small change download
    
    again
    
    see that way
    
    more
    
    wired
    
    removed auth
    
    remove env
    
    Revert "remove env"
    
    This reverts commit 7a6f71fbb0fa8a13e0b73185c763d6b6f96814c0.
    
    Revert "removed auth"
    
    This reverts commit 801767dbd5bac66839cdb518fb40cc74202909aa.
    
    old style
    
    added mac
    
    direct changed
    
    fix
    
    I will write poem here
    
    finally build but its not the end :D
    
    too fast
    
    again
    
    shorter one :D
    
    and again
    
    single one
    
    full name
    
    download
    
    equal
    
    notarise strings
    
    notarize
    
    osx
    
    boring
    
    wiered once working once not
    
    removed token
    
    Try that one
    
    white spaces
    
    only osx
    
    disabled auth
    
    idk
    
    precision
    
    update packages
    
    boring
    
    osx
    
    idk

--- build/notarize.js
+++ build/notarize.js
@@ -1,25 +1,40 @@
-require("dotenv").config()
-const { notarize } = require("electron-notarize")
+require("dotenv").config();
+const { notarize } = require("electron-notarize");
 
 /*
  Pre-requisites: https://github.com/electron/electron-notarize#prerequisites
     1. Generate an app specific password
-    2. Add ELECTRON_WALLET_APPLE_ID, ELECTRON_WALLET_APP_PASSWORD, ELECTRON_WALLET_TEAM_ID to .env file in the root directory (where quasar.conf.js is located)
+    2. Add SIGNING_APPLE_ID, SIGNING_APP_PASSWORD, SIGNING_TEAM_ID to .env file in the root directory (where quasar.conf.js is located)
 */
 
-exports.default = async function notarizing (context) {
-    const { electronPlatformName, appOutDir } = context
-    if (electronPlatformName !== "darwin") {
-        return
-    }
+/*
+  Notarizing: https://kilianvalkhof.com/2019/electron/notarizing-your-electron-application/
+*/
 
-    const appName = context.packager.appInfo.productFilename
+const log = msg => console.log(`\n${msg}`);
+const isEmpty = v => !v || v.length === 0;
 
-    return notarize({
-        appBundleId: "com.arqma.electron-wallet",
-        appPath: `${appOutDir}/${appName}.app`,
-        appleId: process.env.ELECTRON_WALLET_APPLE_ID,
-        appleIdPassword: process.env.ELECTRON_WALLET_PASSWORD,
-        ascProvider: process.env.ELECTRON_WALLET_TEAM_ID
-    })
-}
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;
+  if (electronPlatformName !== "darwin") {
+    return;
+  }
+  log("Notarizing mac application");
+
+  const appName = context.packager.appInfo.productFilename;
+  const { ELECTRON_WALLET_APPLE_ID, ELECTRON_WALLET_PASSWORD, ELECTRON_WALLET_TEAM_ID } = process.env;
+
+  if (isEmpty(ELECTRON_WALLET_APPLE_ID) || isEmpty(ELECTRON_WALLET_PASSWORD)) {
+    log("SIGNING_APPLE_ID or SIGNING_APP_PASSWORD not set.\nTerminating noratization.");
+    return;
+  }
+
+  const options = {
+    appBundleId: "com.arqma-project.electron-wallet",
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: ELECTRON_WALLET_APPLE_ID,
+    appleIdPassword: ELECTRON_WALLET_PASSWORD
+  };
+  if (!isEmpty(ELECTRON_WALLET_TEAM_ID)) options.ascProvider = ELECTRON_WALLET_TEAM_ID;
+  return notarize(options);
+};
