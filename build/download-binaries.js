26054e6490639c259da1487e88ebaa5404ff08cf
ArqTras <biuro@ar-net.info>
2020-07-11 00:17:51 +0200

    small change download

--- build/download-binaries.js
+++ build/download-binaries.js
@@ -25,7 +25,7 @@ async function download () {
             .map(asset => asset["browser_download_url"])
             .find(url => {
                 if (platform === "darwin") {
-                    return url.includes("arqma-osx-x64-v0.6.2.zip") || url.includes("arqma-osx-x64")
+                    return url.includes("osx-x64-v0.6.2.zip")
                 } else if (platform === "win32") {
                     return url.includes("win64") || url.includes("win64")
                 }
