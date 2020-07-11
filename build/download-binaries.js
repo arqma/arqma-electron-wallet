fe0f9eb0919a11cd5a60e6f88d1aea6cf917f6e7
ArqTras <biuro@ar-net.info>
2020-07-11 22:31:51 +0200

    I will write poem here

--- build/download-binaries.js
+++ build/download-binaries.js
@@ -18,7 +18,7 @@ async function download () {
             .map(asset => asset["browser_download_url"])
             .find(url => {
                 if (platform === "darwin") {
-                      return url.includes("osx") || url.includes("osx")
+                      return url.includes("osx") || url.includes("osx.zip")
                 } else if (platform === "win32") {
                     return url.includes("win64") || url.includes("win64")
                 }
