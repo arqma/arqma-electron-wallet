801767dbd5bac66839cdb518fb40cc74202909aa
ArqTras <biuro@ar-net.info>
2020-07-11 21:35:14 +0200

    removed auth

--- build/download-binaries.js
+++ build/download-binaries.js
@@ -3,22 +3,14 @@ const fs = require("fs-extra");
 const path = require("path");
 
 async function download() {
-  const { platform, env } = process;
+  const { platform } = process;
   const repoUrl = "https://api.github.com/repos/arqma/arqma/releases/latest";
   try {
     const pwd = process.cwd();
     const downloadDir = path.join(pwd, "downloads");
     await fs.ensureDir(downloadDir);
 
-    const headers = {
-      "Content-Type": "application/json",
-      "User-Agent": "Arqma-Electron-Wallet"
-    };
-    if (env.GH_TOKEN) {
-      headers.Authorisation = `Bearer ${env.GH_TOKEN}`;
-    }
-
-    const { data } = await axios.get(repoUrl, { headers });
+    const { data } = await axios.get(repoUrl);
     const { name } = data;
     console.log("Latest release: " + name);
 
